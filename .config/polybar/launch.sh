5be09db3639338cdd86ff411fc6e42b152a210db
keer2345 <keer2345@gmail.com>
2021-04-13 00:00:22 +0800

    polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# chmod +x launch.sh
+
+# set correct resolution ratio
+# xrandr && xrandr --output eDP-1 --mode 1600x900
+
+# load background
+# feh --bg-fill ~/Media/Bilder/ArenaBlurArchFullHD.png
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if [[ $(xrandr -q | grep '^DP1 connected') ]]; then
+    MONITOR=DP1 polybar --reload top &
+else
+    MONITOR=eDP1 polybar --reload top &
+fi
+
+## loop monitors
+# for m in $(polybar --list-monitors | cut -d":" -f1); do
+    # MONITOR=$m polybar --reload top &
+# done
+
+# MONITOR=eDP1 polybar --reload top &
